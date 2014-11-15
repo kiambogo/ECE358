55597b28fabf881a957bff8bde3f3d1b2d9b1e4d
Paul Trautrim <pctrautr@uwaterloo.ca>
2014-11-15 16:38:04 -0500

    Add outlines for main and the simulation class.

---
+++ Project 2/src/main.cpp
@@ -0,0 +1,32 @@
+#include "simulation.hpp"
+
+void question1();
+
+int main()
+{
+	question1();
+
+	return 0;
+}
+
+void question1()
+{
+	// N=20, A=5, P=1, time=60s
+	simulation *sim = new simulation(20, 5, 1, 60);
+	// N=40, A=5, P=1, time=60s
+	// N=60, A=5, P=1, time=60s
+	// N=80, A=5, P=1, time=60s
+	// N=100, A=5, P=1, time=60s
+
+	// N=20, A=6, P=1, time=60s
+	// N=40, A=6, P=1, time=60s
+	// N=60, A=6, P=1, time=60s
+	// N=80, A=6, P=1, time=60s
+	// N=100, A=6, P=1, time=60s
+
+	// N=20, A=7, P=1, time=60s
+	// N=40, A=7, P=1, time=60s
+	// N=60, A=7, P=1, time=60s
+	// N=80, A=7, P=1, time=60s
+	// N=100, A=7, P=1, time=60s
+}
---
+++ Project 2/src/simulation.hpp
@@ -0,0 +1,56 @@
+#include <list>
+#include <map>
+#include "host.hpp"
+#include "signal.hpp"
+
+class simulation
+{
+public:
+	const unsigned int n;
+	const unsigned int a;
+	static const unsigned int w = 1000000; // 1 Mbps
+	static const unsigned int l = 1500;
+	const unsigned int p;
+
+	const static double tick_length = 50 * 10^-9; // seconds
+
+	simulation(unsigned int n, unsigned int a, unsigned int p, unsigned int run_time) : n(n), a(a), p(p), run_time(run_time) {};
+	void run();
+
+private:
+	unsigned int ticks;
+	unsigned int run_time;
+
+	medium *network;
+	std::list<host> active_hosts;
+	std::map<unsigned int, host> generated_packets;
+
+	void init();
+	void tick();
+};
+
+void simulation::run()
+{
+	init();
+	for (ticks = 0; ticks < run_time / tick_length; ticks++) {
+		tick();
+	}
+}
+
+void simulation::init()
+{
+	// Create medium
+
+	// Create hosts
+
+	// Fill generated_packets map
+}
+
+void simulation::tick()
+{
+	// Propagate signals
+
+	// Check for generated packets / mark hosts active
+
+	// Run host logic for each active host
+}
