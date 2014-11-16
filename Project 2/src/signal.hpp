825b08d915e599d4efee4075a62883a53752a608
Paul Trautrim <pctrautr@uwaterloo.ca>
2014-11-15 19:05:17 -0500

    Update signal.hpp now that medium.hpp has the propagation logic.

--- Project 2/src/signal.hpp
+++ Project 2/src/signal.hpp
@@ -5,12 +5,8 @@ class signal
 public:
 	enum direction {LEFT = -1, RIGHT = 1};
 	bool jamming;
-
-	signal(int pos, bool jamming, direction dir, unsigned int n) : pos(pos), jamming(jamming), dir(dir), n(n) {};
-	void propagate();
-
-private:
 	const direction dir;
 	int pos;
-	const int n;
+
+	signal(int pos, bool jamming, direction dir) : pos(pos), jamming(jamming), dir(dir) {};
 };
