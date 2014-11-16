46977441f2ab7e32a3ede969d8a3bddd8faaae35
Paul Trautrim <pctrautr@uwaterloo.ca>
2014-11-15 21:22:16 -0500

    Change to C++11 and add work on init().

--- Project 2/src/simulation.cpp
+++ Project 2/src/simulation.cpp
@@ -1,8 +1,10 @@
 #include <list>
 #include <map>
 #include <cmath>
+#include <random>
 #include "simulation.hpp"
 #include "host.hpp"
+#include "medium.hpp"
 
 void simulation::run()
 {
@@ -15,23 +17,31 @@ void simulation::run()
 void simulation::init()
 {
 	// Create medium
+	network = new medium(200000000, this);
 
 	// Create hosts
 
-	/**host all_hosts [n] = {}
+	for (int i = 0; i < n; i++) {
+		all_hosts.push_back(host());
+	}
 
 	// Fill generated_packets map
+	unsigned int next_packet_generation_tick = 0;
 	for (int i = 0; i < n-1; i++) {
-		int next_packet = ((-1/a) * log(rand() % 1)) * (1/tick_length)
-		while(next_packet < (1/tick_length)) {
-			generated_packets.insert(next_packet, all_hosts[i])
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<> dis(0, 1);
+		next_packet_generation_tick += ((-1/a) * log(dis(gen) - 1)) * (1/tick_length) + 1;
+		while (next_packet_generation_tick < (run_time / tick_length)) {
+			generated_packets.insert(std::pair<unsigned int,host>(next_packet_generation_tick, all_hosts[i]));
 		}
-	}*/
+	}
 }
 
 void simulation::tick()
 {
 	// Propagate signals
+	network->propagate();
 
 	// Check for generated packets / mark hosts active
 
--- Project 2/src/simulation.hpp
+++ Project 2/src/simulation.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <list>
+#include <vector>
 #include <map>
 #include <cmath>
 #include "host.hpp"
@@ -13,13 +13,13 @@ class simulation
 public:
 	const unsigned int n;
 	const unsigned int a;
-	static const unsigned int w = 1000000; // 1 Mbps
-	static const unsigned int l = 1500;
+	const unsigned int w = 1000000; // 1 Mbps
+	const unsigned int l = 1500;
 	const unsigned int p;
 
 	static const unsigned int distance_between_nodes = 10;
 
-	const static double tick_length = 50 * 10^-9; // seconds
+	const double tick_length = 50 * 10^-9; // seconds
 
 	simulation(unsigned int n, unsigned int a, unsigned int p, unsigned int run_time) : n(n), a(a), p(p), run_time(run_time) {};
 	void run();
@@ -29,8 +29,9 @@ private:
 	unsigned int run_time;
 
 	medium *network;
-	std::list<host> active_hosts;
-	std::map<unsigned int, host> generated_packets;
+	std::vector<host> all_hosts;
+	std::vector<host> active_hosts;
+	std::multimap<unsigned int, host> generated_packets;
 
 	void init();
 	void tick();
